9201bdc81935c8c4cd2ddee7f7f6318bf42a277a
FMDcoder <76693953+FMDcoder@users.noreply.github.com>
2023-12-17 23:24:56 +0100

    first commit

---
+++ GruppProjekt Grupp16 CV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GruppProjekt_Grupp16_CV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
