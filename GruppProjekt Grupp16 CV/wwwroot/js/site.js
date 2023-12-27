7b2b1f58170b83c449682ecc4f23060d51388371
FMDcoder <76693953+FMDcoder@users.noreply.github.com>
2023-12-27 23:54:22 +0100

    a

--- GruppProjekt Grupp16 CV/wwwroot/js/site.js
+++ GruppProjekt Grupp16 CV/wwwroot/js/site.js
@@ -1,13 +1,1 @@
-﻿ew Vue({
-    el: '#registerApp',
-    data: {
-        imageUrl: ''
-    },
-    methods: {
-        updateImage() {
-            if (!this.imageUrl) {
-                this.imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Person_icon_BLACK-01.svg/1924px-Person_icon_BLACK-01.svg.png';
-            }
-        }
-    }
-});
+﻿
